ea2de0b075ccc5210ddc41abf42db6572bbb4702
Leandro Nacimento <leandronacimento04@gmail.com>
2026-04-24 22:42:07 -0300

    refactor: read admin seeder credentials from config

---
+++ config/admin-user.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name' => env('ADMIN_NAME', 'Admin Ivonne'),
+    'email' => env('ADMIN_EMAIL', 'admin@ivonneshowroom.com'),
+    'password' => env('ADMIN_PASSWORD', 'password'),
+];
